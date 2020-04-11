f923c48d9990505d3ab8b23d5fc0cdae57666832
CandyMan <c.mattson318@gmail.com>
2020-04-10 21:15:24 -0700

    add PORT variable

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var PORT = process.env.PORT || 3000;
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
